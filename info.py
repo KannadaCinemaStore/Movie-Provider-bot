86d5a631e83f28c4ef52ab19aca76cebb4c0cb19
KannadaCinemaStore <kannadacinemastore@gmail.com>
2025-01-08 21:30:35 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,8 +112,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://leading-anthea-kannadamovie.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://leading-anthea-kannadamovie.koyeb.app/:8000/".format(FQDN, PORT)
+URL = "https://rare-bobina-ravikumar1.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "http://rare-bobina-ravikumar1.koyeb.app/:8000/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'LazyBot'))
