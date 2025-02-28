09a5c959dcd790348bdd0eaecd27fae0c36d354d
KannadaCinemaStore <kannadacinemastore@gmail.com>
2025-02-28 12:45:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8
 CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
 
 #stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'offerlinks.in'))
-STREAM_API = (environ.get('STREAM_API', '9f442a27f749d8d58a79cbc0a1a346d109e5ba22'))
+STREAM_SITE = (environ.get('STREAM_SITE', 'modijiurl.com'))
+STREAM_API = (environ.get('STREAM_API', 'edf836de1a44f5e3d13895f00d64ce32482bd7f7'))
 STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/How_To_Open_Links_bypass'))
 
 # Admins, Channels & Users
